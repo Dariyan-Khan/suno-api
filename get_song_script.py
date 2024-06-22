2232cb0b1654a5a424cc868467bc096fe18438de
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-06-22 22:04:08 +0100

    added playwright stuff

--- get_song_script.py
+++ get_song_script.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from playwright.sync_api import sync_playwright
 
 # replace your vercel domain
 base_url = 'http://localhost:3000'
@@ -45,23 +46,58 @@ def generate_whole_song(clip_id):
     return response.json()
 
 
+import re
+from playwright.sync_api import Page, expect
+
+
+
+def download_song():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page(accept_downloads=True)
+        download_song_inner(page)
+        browser.close()
+
+
+# def download_song_inner(page: Page) -> None:
+def download_song_inner(page: Page) -> None:
+    page.goto("https://suno.com/")
+    page.get_by_text("Sign Up / Log In").click()
+    page.locator(".cl-socialButtons > button:nth-child(2)").click()
+    page.get_by_label("Email or Phone Number*").click()
+    page.get_by_label("Email or Phone Number*").fill("dariyankhan01@gmail.com")
+    page.get_by_label("Password*").click()
+    page.get_by_label("Password*").fill("choco46D")
+    page.get_by_role("button", name="Log In").click()
+    page.goto("https://suno.com/")
+    page.goto("https://suno.com/song/b6b23513-7e02-4192-8349-081c77ff62e8")
+    page.locator("div").filter(has_text=re.compile(r"^10Share SongReuse Promptv3\.522 June 2024 at 21:03$")).get_by_label("More Actions").click()
+    with page.expect_download() as download_info:
+        page.get_by_test_id("download-audio-menu-item").click()
+    download = download_info.value
+
+    download.save_as("/songs_generated" + download.suggested_filename)
+
+
 
 
 if __name__ == '__main__':
-    data = generate_audio_by_prompt({
-        "prompt": "A popular heavy metal song about war, sung by a deep-voiced male singer, slowly and melodiously. The lyrics depict the sorrow of people after the war.",
-        "make_instrumental": False,
-        "wait_audio": False
-    })
+    # data = generate_audio_by_prompt({
+    #     "prompt": "An r&b song about fire",
+    #     "make_instrumental": False,
+    #     "wait_audio": False
+    # })
 
-    ids = f"{data[0]['id']},{data[1]['id']}"
-    print(f"ids: {ids}")
+    # ids = f"{data[0]['id']},{data[1]['id']}"
+    # print(f"ids: {ids}")
 
-    for _ in range(60):
-        data = get_audio_information(ids)
-        if data[0]["status"] == 'streaming':
-            print(f"{data[0]['id']} ==> {data[0]['audio_url']}")
-            print(f"{data[1]['id']} ==> {data[1]['audio_url']}")
-            break
-        # sleep 5s
-        time.sleep(5)
+    # for _ in range(60):
+    #     data = get_audio_information(ids)
+    #     if data[0]["status"] == 'streaming':
+    #         print(f"{data[0]['id']} ==> {data[0]['audio_url']}")
+    #         print(f"{data[1]['id']} ==> {data[1]['audio_url']}")
+    #         break
+    #     # sleep 5s
+    #     time.sleep(5)
+
+    download_song()
