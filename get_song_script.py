b9f544ae6a468cc9f86adc23ae9ffd8c1b6487aa
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-06-27 20:37:20 +0100

    download feature added and working

--- get_song_script.py
+++ get_song_script.py
@@ -2,6 +2,7 @@ import time
 import requests
 from playwright.sync_api import sync_playwright, Page, expect
 import re
+import os
 
 # replace your vercel domain
 base_url = 'http://localhost:3000'
@@ -47,8 +48,27 @@ def generate_whole_song(clip_id):
     return response.json()
 
 
+# def get_song(self, id: str):
+#         """
+#         Retrieve a single song by its ID.
 
-def download(self, song: str | Clip, path: str = "./downloads",) -> str:
+#         Parameters:
+#         - id (str): The ID of the song to retrieve.
+
+#         Returns:
+#         Clip: A Clip object containing details about the song, such as the audio URL, song status, and other metadata.
+#         """
+#         self._keep_alive()  # Ensure session is active
+#         logger.info("Getting Song Info...")
+#         response = self.client.get(
+#             f"{Suno.BASE_URL}/api/feed/?ids={id}")  # Call API
+#         logger.debug(response.text)
+#         self._cehck_error(response)
+#         return create_clip_from_data(response.json()[0])
+
+
+
+def download(song: str, path: str = "../songs_generated",) -> str:
         """
         Downloads a Suno song to a specified location.
 
@@ -63,85 +83,57 @@ def download(self, song: str | Clip, path: str = "./downloads",) -> str:
             TypeError: If the 'song' argument is not of type str or Clip.
             Exception: If the download fails (e.g., bad URL, HTTP errors).
         """
-        if isinstance(song, Clip):
-            id = song.id
-            url = song.audio_url
-        elif isinstance(song, str):
+        
+        if isinstance(song, str):
             id = song
-            url = self.get_song(id).audio_url
+            # url = self.get_song(id).audio_url
+            #url = "https://audiopipe.suno.ai/?" + song
+            url = f"https://cdn1.suno.ai/{id}.mp3"
         else:
             raise TypeError
-        logger.info(f"Audio URL : {url}")
+        
+        print(f"Audio URL : {url}")
+        
+        #logger.info(f"Audio URL : {url}")
         response = requests.get(url)
         if not response.ok:
             raise Exception(
                 f"failed to download from audio url: {response.status_code}"
             )
-        response = requests.get(url, stream=True)
-        response.raise_for_status()  # Check for HTTP errors
-        filename = self._get_dl_path(id, path)
-        with open(filename, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:  # Filter out keep-alive chunks
-                    f.write(chunk)
-        logger.info(f"Download complete: {filename}")
-        return filename
+        
+        # assert False
 
 
-# def download_song():
-#     with sync_playwright() as p:
-#         browser = p.chromium.launch(headless=False)
-#         page = browser.new_page(accept_downloads=True)
-#         download_song_inner(page)
-#         browser.close()
-
-
-# # def download_song_inner(page: Page) -> None:
-# def download_song_inner():
-#     with sync_playwright() as p:
-#         browser = p.chromium.launch(headless=False)
-#         context = browser.new_context(accept_downloads=True)
-#         page = context.new_page()
-#         page.goto("https://suno.com/")
-#         page.get_by_text("Sign Up / Log In").click()
-#         page.locator(".cl-socialButtons > button:nth-child(2)").click()
-#         page.get_by_label("Email or Phone Number*").click()
-#         page.get_by_label("Email or Phone Number*").fill("dariyankhan01@gmail.com")
-#         page.get_by_label("Password*").click()
-#         page.get_by_label("Password*").fill("choco46D")
-#         page.get_by_role("button", name="Log In").click()
-
-#         page2 = context.new_page()
-#         page2.goto("https://suno.com/")
-#         page2.goto("https://suno.com/song/b6b23513-7e02-4192-8349-081c77ff62e8")
-#         page2.locator("div").filter(has_text=re.compile(r"^10Share SongReuse Promptv3\.522 June 2024 at 21:03$")).get_by_label("More Actions").click()
-#         with page2.expect_download() as download_info:
-#             page2.get_by_test_id("download-audio-menu-item").click()
-#         download = download_info.value
-
-#         download.save_as("/songs_generated" + download.suggested_filename)
+        file = os.path.join(path, f"{id}.mp3")
+        with open(file, "wb") as f:
+            f.write(response.content)
+        print(f"audio file: {file}")
 
 
 
 
 if __name__ == '__main__':
-    data = generate_audio_by_prompt({
-        "prompt": "An r&b song about Cardiff",
-        "make_instrumental": False,
-        "wait_audio": False
-    })
+    # data = generate_audio_by_prompt({
+    #     "prompt": "An r&b song about Cardiff",
+    #     "make_instrumental": False,
+    #     "wait_audio": False
+    # })
 
-    ids = f"{data[0]['id']},{data[1]['id']}"
-    print(f"ids: {ids}")
+    # ids = f"{data[0]['id']},{data[1]['id']}"
+    # print(f"ids: {ids}")
 
-    for _ in range(60):
-        data = get_audio_information(ids)
-        if data[0]["status"] == 'streaming':
-            print(f"{data[0]['id']} ==> {data[0]['audio_url']}")
-            print(f"{data[1]['id']} ==> {data[1]['audio_url']}")
-            break
-        # sleep 5s
-        time.sleep(5)
+    # for _ in range(60):
+    #     data = get_audio_information(ids)
+    #     if data[0]["status"] == 'streaming':
+    #         print(f"{data[0]['id']} ==> {data[0]['audio_url']}")
+    #         print(f"{data[1]['id']} ==> {data[1]['audio_url']}")
+    #         break
+    #     # sleep 5s
+    #     time.sleep(5)
+
+    test_id = "81ec34ab-da9f-478e-8266-526035b0cb51"
+
+    download(test_id)
 
     # download_song_inner()
 
